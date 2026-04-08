4c6a0a5649ed99621b85b3e3ae04cfdcff702f1e
Nilesh Godase <nileshgodase@gmail.com>
2026-04-08 23:27:44 +0530

    Role based middleware created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
-const authMiddleware = require('../middlewares/auth.middleware');
+const authMiddleware = require("../middlewares/auth.middleware");
+const authorizeRoles = require("../middlewares/role.middleware");
 
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 
-//test purpose only
-router.get('/profile', authMiddleware, (req, res) => {
-  res.json({
-    message: 'Protected route',
-    user: req.user
-  });
-});
+// role based api test
+router.get(
+  "/admin-only",
+  authMiddleware,
+  authorizeRoles("admin"),
+  (req, res) => {
+    res.json({ message: "Welcome Admin" });
+  },
+);
+
+router.get(
+  "/common",
+  authMiddleware,
+  authorizeRoles("admin", "user"),
+  (req, res) => {
+    res.json({ message: "All logged-in users allowed" });
+  },
+);
 
 module.exports = router;
